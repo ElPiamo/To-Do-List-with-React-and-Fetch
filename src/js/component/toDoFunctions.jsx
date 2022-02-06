0c50b281205c5d3c4f42365d382d0a0b5454045b
unknown <rauljosepiamo@gmail.com>
2022-02-06 11:05:02 -0400

    final commit to deliver assignement

--- src/js/component/toDoFunctions.jsx
+++ src/js/component/toDoFunctions.jsx
@@ -1,44 +1,120 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 const AllToDos = () => {
 	const [input, setInput] = useState("");
 
-	const [todo, setTodo] = useState([]);
-
-	const handleKeyDown = e => {
-		if (e.key == "Enter") {
-			if (input.trim() != "") {
-				setTodo([...todo, input]);
-			}
-			setInput("");
+	const [todo, setTodo] = useState([
+		{
+			label: "",
+			done: false
 		}
-	};
+	]);
 
-	const handleDeleteTask = idtodelete => {
-		setTodo(todo => todo.filter((tarea, i) => i !== idtodelete));
-	};
 	const [isHovering, setIsHovering] = useState(-1);
-	const [xOver, setXOver] = useState(false);
-	const glowUp = () => {
-		if (xOver == true) {
-			return { boxShadow: "0px 0px 20px 10px #babff5" };
+
+	const urlBase = "https://assets.breatheco.de/apis/fake/todos/user";
+
+	const verifyUser = async () => {
+		try {
+			let response = await fetch(`${urlBase}/ElPiamo`, {
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+			if (response.ok) {
+				getTasks();
+			}
+		} catch (error) {
+			console.log(error, "explote sorry");
 		}
 	};
+
+	const getTasks = async () => {
+		try {
+			let response = await fetch(`${urlBase}/ElPiamo`);
+			if (response.ok) {
+				let resultado = await response.json();
+				setTodo(resultado);
+				setInput("");
+			} else {
+				verifyUser();
+			}
+		} catch (error) {
+			console.log(error, "explote sorry");
+		}
+	};
+	const addTasks = async e => {
+		try {
+			if (e.key == "Enter") {
+				if (input.trim() != "") {
+					let response = await fetch(`${urlBase}/ElPiamo`, {
+						method: "PUT",
+						body: JSON.stringify([
+							...todo,
+							{ label: input, done: false }
+						]),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					if (response.ok) {
+						getTasks();
+					}
+				}
+			}
+		} catch (error) {
+			console.log(error, "explote sorry");
+		}
+	};
+
+	const deleteTasks = async idtodelete => {
+		try {
+			const newTodo = todo.filter((tarea, i) => i !== idtodelete);
+			let response = await fetch(`${urlBase}/ElPiamo`, {
+				method: "PUT",
+				body: JSON.stringify(newTodo),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+			if (response.ok) {
+				getTasks();
+			}
+		} catch (error) {
+			console.log(error, "explote sorry");
+		}
+	};
+	useEffect(() => {
+		getTasks();
+	}, []);
 	return (
 		<div className="text-center">
 			<h1 className="mainTitle">{`To Do's`}</h1>
 			<div className="theFormat">
 				<ul className="tasksLists">
 					<form onSubmit={e => e.preventDefault()}>
-						<input
-							className="inputData"
-							type="text"
-							placeholder="Type here what needs to be done"
-							value={input}
-							name="text"
-							onChange={e => setInput(e.target.value)}
-							onKeyDown={handleKeyDown}
-						/>
+						{todo.length == 0 ? (
+							<input
+								className="inputData"
+								type="text"
+								placeholder="No tasks, add a task"
+								value={input}
+								name="text"
+								onChange={e => setInput(e.target.value)}
+								onKeyDown={addTasks}
+							/>
+						) : (
+							<input
+								className="inputData"
+								type="text"
+								placeholder="Type the tasks that need to be done"
+								value={input}
+								name="text"
+								onChange={e => setInput(e.target.value)}
+								onKeyDown={addTasks}
+							/>
+						)}
 					</form>
 					{todo.map((tarea, i) => {
 						return (
@@ -48,27 +124,18 @@ const AllToDos = () => {
 								onMouseEnter={() => setIsHovering(i)}
 								onMouseLeave={() => setIsHovering(-1)}>
 								{" "}
-								{tarea}
+								{tarea.label}
 								<div
 									onClick={() => {
-										handleDeleteTask(i);
+										deleteTasks(i);
 									}}
 									className={
 										isHovering === i
 											? "text-danger"
-											: "taskNoHovering"
+											: "taskNoHovering text-light"
 									}>
-									X
+									<i className="fas fa-trash-alt "></i>
 								</div>
-								{/* <i									
-									onClick={() => {
-										handleDeleteTask(i);
-									}}
-									className={
-										isHovering === i
-											? "fas fa-window-close"
-											: "taskNoHovering"
-									}></i> */}
 							</div>
 						);
 					})}
@@ -82,5 +149,4 @@ const AllToDos = () => {
 		</div>
 	);
 };
-
 export default AllToDos;
